f0ca416c0fc3e47663cdb2f139a49db9d8a458c6
Saul Espinosa Ruiz <rinku.enlace@gmail.com>
2023-11-13 09:40:06 -0600

    feat: create the store method

--- app/Http/Controllers/Api/MovieController.php
+++ app/Http/Controllers/Api/MovieController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\MovieRequest;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
@@ -20,7 +21,16 @@ class MovieController extends Controller
 
     public function store(MovieRequest $request)
     {
+        $path = $request->picture_file_path->store('images', 'public');
+        $movie = Movie::create([
+            'name' => $request->name,
+            'release_date' => $request->release_date,
+            'is_active' => $request->is_active,
+            'picture_file_path' => $path,
+            'minutes_length' => $request->minutes_length,
+        ]);
 
+        return response()->json($movie);
     }
 
     public function show(string $id)
