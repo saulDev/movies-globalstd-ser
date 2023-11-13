c8790167be6d059e917f5f19d56839113106b6bd
Saul Espinosa Ruiz <rinku.enlace@gmail.com>
2023-11-13 11:55:03 -0600

    feat: new auth methods

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,5 +9,13 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'v1', 'middleware' => 'auth:sanctum'], function () {
 
     Route::post('movies', [MovieController::class, 'store']);
+    Route::get('user', [AuthController::class, 'user']);
+
+});
+
+Route::group(['prefix' => 'v1', 'middleware' => 'api'], function () {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
 
 });
